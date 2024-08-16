363c26903b428c6ce692dfc1b5d923e085d7dee5
jpops <jptrs93@gmail.com>
2024-08-16 22:19:35 +0100

    add fileutils func

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -3,6 +3,8 @@ package fileutil
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func AppendTo(fileName string, data []byte) error {
@@ -27,4 +29,24 @@ func MustEnsureDir(path string) {
 	if err := os.MkdirAll(path, os.ModePerm); err != nil {
 		panic(fmt.Sprintf("failed creating dir %v: %v", path, err))
 	}
+}
+
+func RemoveFilesWithPrefix(dir string, filePrefix string) error {
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && strings.HasPrefix(info.Name(), filePrefix) {
+			err := os.Remove(path)
+			if err != nil {
+				return fmt.Errorf("failed to remove file %s: %w", path, err)
+			}
+		}
+
+		return nil
+	})
+
+	return err
 }
