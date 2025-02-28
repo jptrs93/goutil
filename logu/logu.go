7ddffd2f5c50e00c0c410ada2c9ad077d01b8c2a
jpops <jptrs93@gmail.com>
2025-02-28 12:08:18 +1100

    update logu

--- logu/logu.go
+++ logu/logu.go
@@ -6,26 +6,25 @@ import (
 	"github.com/jptrs93/goutil/timeu"
 	"io"
 	"log/slog"
+	"strings"
 )
 
 const LogContextKey = "_logContext"
 
-func ExtendLogContext(ctx context.Context, additionalContext string) context.Context {
-	if existingLogContext, ok := ctx.Value(LogContextKey).(string); ok {
-		if len(existingLogContext) > 0 {
-			return context.WithValue(ctx, LogContextKey, existingLogContext+" "+additionalContext)
-		} else {
-			return context.WithValue(ctx, LogContextKey, additionalContext)
-		}
+func ExtendLogContext(ctx context.Context, key string, value any) context.Context {
+	if existingLogContext, ok := ctx.Value(LogContextKey).(map[string]any); ok {
+		existingLogContext[key] = value
+		return context.WithValue(ctx, LogContextKey, existingLogContext)
 	}
-	return context.WithValue(ctx, LogContextKey, additionalContext)
+	logContext := map[string]any{key: value}
+	return context.WithValue(ctx, LogContextKey, logContext)
 }
 
-func GetLogContext(ctx context.Context) string {
-	if existingLogContext, ok := ctx.Value(LogContextKey).(string); ok {
-		return existingLogContext
+func GetLogContext(ctx context.Context) map[string]any {
+	if m, ok := ctx.Value(LogContextKey).(map[string]any); ok {
+		return m
 	}
-	return ""
+	return nil
 }
 
 type PlainLogHandler struct {
@@ -38,12 +37,8 @@ func (h *PlainLogHandler) Enabled(ctx context.Context, level slog.Level) bool {
 }
 
 func (h *PlainLogHandler) Handle(ctx context.Context, r slog.Record) error {
-	logContext := ""
-	if ctx != nil {
-		if v, ok := ctx.Value(LogContextKey).(string); ok {
-			logContext = " " + v
-		}
-	}
+	logContext := buildStrContext(GetLogContext(ctx))
+
 	// make the level strings all the same length
 	var levelStr string
 	switch r.Level {
@@ -56,9 +51,23 @@ func (h *PlainLogHandler) Handle(ctx context.Context, r slog.Record) error {
 	case slog.LevelError:
 		levelStr = "ERROR"
 	}
-	timestamp := r.Time.UTC().Format(timeu.RFC3339Milli)
-	msg := fmt.Sprintf("%s %s%s %s\n", timestamp, levelStr, logContext, r.Message)
+	var stacktrace string
+	r.Attrs(func(a slog.Attr) bool {
+		if a.Key == "stacktrace" {
+			stacktrace = a.Value.String()
+			return false
+		}
+		return true
+	})
+	msg := fmt.Sprintf("%s %s%s %s\n", r.Time.UTC().Format(timeu.RFC3339Milli), levelStr, logContext, r.Message)
+
 	_, err := fmt.Fprint(h.Writer, msg)
+	if err != nil {
+		return err
+	}
+	if stacktrace != "" {
+		_, err = fmt.Fprint(h.Writer, stacktrace)
+	}
 	return err
 }
 
@@ -68,4 +77,15 @@ func (h *PlainLogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 
 func (h *PlainLogHandler) WithGroup(name string) slog.Handler {
 	return h
+}
+
+func buildStrContext(logCtx map[string]any) string {
+	if len(logCtx) == 0 {
+		return ""
+	}
+	vals := make([]string, 0, len(logCtx))
+	for k, v := range logCtx {
+		vals = append(vals, fmt.Sprintf("%v=%v", k, v))
+	}
+	return " [" + strings.Join(vals, ", ") + "]"
 }
