24208eee5692b2305d900dfdc38dbab0b0a1240a
Joss Peters <jptrs93@gmail.com>
2025-08-15 09:08:51 +1000

    Add LoadDotEnv func

--- envu/envu.go
+++ envu/envu.go
@@ -3,7 +3,9 @@ package envu
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 )
@@ -63,4 +65,48 @@ func MustGetOrDefault[T any](key string, defaultValue T) T {
 		return defaultValue
 	}
 	return MustGet[T](key)
+}
+
+func LoadDotEnv(missingOk bool) {
+	f, ok := os.LookupEnv("DOT_ENV_FILE")
+	// If DOT_ENV_FILE is not set, search for .env file backwards from current dir
+	if !ok || f == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(fmt.Sprintf("failed to get current directory: %v", err))
+		}
+		dir := cwd
+		for {
+			envPath := filepath.Join(dir, ".env")
+			if _, err := os.Stat(envPath); err == nil {
+				f = envPath
+				break
+			}
+			parent := filepath.Dir(dir)
+			if parent == dir {
+				if missingOk {
+					log.Println("No .env file found")
+					return
+				}
+				panic("no .env file found in current directory or any parent directory")
+			}
+			dir = parent
+		}
+	}
+	data, err := os.ReadFile(f)
+	if err != nil {
+		panic(fmt.Sprintf("failed to read .env file %s: %v", f, err))
+	}
+	env := make(map[string]string)
+	if err := parseDotEnvBytes(data, env); err != nil {
+		panic(fmt.Sprintf("failed to parse .env file %s: %v", f, err))
+	}
+	for key, value := range env {
+		// don't overload existing variables
+		if _, exists := os.LookupEnv(key); !exists {
+			if err := os.Setenv(key, value); err != nil {
+				panic(fmt.Sprintf("failed to set environment variable %s: %v", key, err))
+			}
+		}
+	}
 }
