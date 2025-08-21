dafa4063c743d4d5dfffb388f8e419c9aee6b080
Joss Peters <jptrs93@gmail.com>
2025-08-21 10:27:27 +1000

    Update loadDotEnv

--- envu/envu.go
+++ envu/envu.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"reflect"
 	"strings"
+	"sync"
 )
 
 func IsTestBasedOnArgs() bool {
@@ -67,46 +68,50 @@ func MustGetOrDefault[T any](key string, defaultValue T) T {
 	return MustGet[T](key)
 }
 
+var loadDotEnvOnce = sync.Once{}
+
 func LoadDotEnv(missingOk bool) {
-	f, ok := os.LookupEnv("DOT_ENV_FILE")
-	// If DOT_ENV_FILE is not set, search for .env file backwards from current dir
-	if !ok || f == "" {
-		cwd, err := os.Getwd()
-		if err != nil {
-			panic(fmt.Sprintf("failed to get current directory: %v", err))
-		}
-		dir := cwd
-		for {
-			envPath := filepath.Join(dir, ".env")
-			if _, err := os.Stat(envPath); err == nil {
-				f = envPath
-				break
+	loadDotEnvOnce.Do(func() {
+		f, ok := os.LookupEnv("DOT_ENV_FILE")
+		// If DOT_ENV_FILE is not set, search for .env file backwards from current dir
+		if !ok || f == "" {
+			cwd, err := os.Getwd()
+			if err != nil {
+				panic(fmt.Sprintf("failed to get current directory: %v", err))
 			}
-			parent := filepath.Dir(dir)
-			if parent == dir {
-				if missingOk {
-					log.Println("No .env file found")
-					return
+			dir := cwd
+			for {
+				envPath := filepath.Join(dir, ".env")
+				if _, err := os.Stat(envPath); err == nil {
+					f = envPath
+					break
 				}
-				panic("no .env file found in current directory or any parent directory")
-			}
-			dir = parent
-		}
-	}
-	data, err := os.ReadFile(f)
-	if err != nil {
-		panic(fmt.Sprintf("failed to read .env file %s: %v", f, err))
-	}
-	env := make(map[string]string)
-	if err := parseDotEnvBytes(data, env); err != nil {
-		panic(fmt.Sprintf("failed to parse .env file %s: %v", f, err))
-	}
-	for key, value := range env {
-		// don't overload existing variables
-		if _, exists := os.LookupEnv(key); !exists {
-			if err := os.Setenv(key, value); err != nil {
-				panic(fmt.Sprintf("failed to set environment variable %s: %v", key, err))
+				parent := filepath.Dir(dir)
+				if parent == dir {
+					if missingOk {
+						log.Println("No .env file found")
+						return
+					}
+					panic("no .env file found in current directory or any parent directory")
+				}
+				dir = parent
 			}
 		}
-	}
+		data, err := os.ReadFile(f)
+		if err != nil {
+			panic(fmt.Sprintf("failed to read .env file %s: %v", f, err))
+		}
+		env := make(map[string]string)
+		if err := parseDotEnvBytes(data, env); err != nil {
+			panic(fmt.Sprintf("failed to parse .env file %s: %v", f, err))
+		}
+		for key, value := range env {
+			// don't overload existing variables
+			if _, exists := os.LookupEnv(key); !exists {
+				if err := os.Setenv(key, value); err != nil {
+					panic(fmt.Sprintf("failed to set environment variable %s: %v", key, err))
+				}
+			}
+		}
+	})
 }
