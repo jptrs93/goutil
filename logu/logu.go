451481b5c07bf0cf1a470161fe78f9b306fc2993
Joss Peters <jptrs93@gmail.com>
2025-09-25 09:20:48 +1000

    Add ResolveLogDir

--- logu/logu.go
+++ logu/logu.go
@@ -7,6 +7,9 @@ import (
 	"github.com/jptrs93/goutil/timeu"
 	"io"
 	"log/slog"
+	"os"
+	"path/filepath"
+	"runtime"
 	"strings"
 )
 
@@ -123,4 +126,43 @@ func (h *PlainLogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 
 func (h *PlainLogHandler) WithGroup(name string) slog.Handler {
 	return h
+}
+
+func MustResolveLogDir(appName string) string {
+	if dir, err := ResolveLogDir(appName); err != nil {
+		panic(err)
+	} else {
+		return dir
+	}
+}
+
+func ResolveLogDir(appName string) (string, error) {
+	var baseDir string
+	switch runtime.GOOS {
+	case "darwin":
+		// macOS: ~/Library/Logs
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to get user home directory: %w", err)
+		}
+		baseDir = filepath.Join(home, "Library", "Logs")
+	case "linux":
+		// Linux: Follow XDG Base Directory specification
+		if xdgDataHome := os.Getenv("XDG_DATA_HOME"); xdgDataHome != "" {
+			baseDir = xdgDataHome
+		} else {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				return "", fmt.Errorf("failed to get user home directory: %w", err)
+			}
+			baseDir = filepath.Join(home, ".local", "share")
+		}
+	default:
+		return "", fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
+	}
+	logDir := filepath.Join(baseDir, appName)
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return "", fmt.Errorf("failed to create log directory %s: %w", logDir, err)
+	}
+	return logDir, nil
 }
